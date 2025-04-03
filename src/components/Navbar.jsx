c37450b27b52efca6bda42fd868f8f86d27d8c97
Vahid819 <vahidmomin1819@gmail.com>
2025-04-03 08:54:41 +0530

    add Save and search part in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import { MenubarDemo } from "./Menubar";
 // import Search from "./Search";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 function Navbar() {
   return (
-    <div className="flex gap-4">
-      <MenubarDemo />
+    <div className="w-full flex gap-4 justify-around">
+      <div>
+        <MenubarDemo />
+      </div>
       {/* <Search /> */}
+      <div className="flex items-center gap-2">
+        
+        <Input type="text" placeholder="Search..." className="w-full" />
+      </div>
+      {/* Save data */}
+      <div>
+        <Button className="bg-blue-500 text-white hover:bg-blue-600 transition duration-300 ease-in-out">
+          Save
+        </Button>
+      </div>
     </div>
   );
 }
