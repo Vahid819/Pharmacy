fa07e80fe2f596277b352db2906cdb133dc0de19
Vahid819 <vahidmomin1819@gmail.com>
2025-04-23 19:53:59 +0530

    coustomize navbar as for test

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,42 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 
 function page() {
+  const { user } = useUser();
+  const { isSignedIn } = useUser();
+  const { isLoaded } = useUser();
+
   return (
     <div>
-      <Link href={"/sign-in"}>Sign In</Link>
-      <Link href={"/sign-up"}>Sign Up</Link>
+      {isSignedIn ? (
+        <div>
+          <h1 className="text-3xl font-bold">Welcome back!</h1>
+          {!isLoaded ? <p>Loadding....</p> : <UserButton />}
+          {/* <UserButton /> */}
+        </div>
+      ) : (
+        <div>
+          {!isLoaded ? (
+            <p>Loadding.....</p>
+          ) : (
+            <div>
+              <Link href={"/sign-in"}>Sign In</Link>
+              <Link href={"/sign-up"}>Sign Up</Link>
+            </div>
+          )}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
