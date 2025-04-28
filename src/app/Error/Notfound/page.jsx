b1cf0c57218af8d944aa4420ba77b332d687c7ba
Vahid819 <vahidmomin1819@gmail.com>
2025-04-28 22:14:26 +0530

    customize error page

--- src/app/Error/Notfound/page.jsx
+++ src/app/Error/Notfound/page.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
-function Notfound() {
+function page() {
   return (
     <div>
-        <h1 className="text-2xl font-bold">User Not Found</h1>
-        <p className="text-gray-500">Please check the user ID and try again.</p>
-        <Link href="/Dashboard/Users" className="text-blue-500 hover:underline">Go back to Users</Link>
+        <h1 className="text-3xl font-bold underline text-center">404</h1>
+        <h2 className="text-2xl font-bold text-center">This page is not Found</h2>
+        <p className="text-lg text-center">Please check the URL or go back to the homepage.</p>
+        <div className="flex justify-center mt-4">
+            <Link href="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Go to Homepage</Link>
+            </div>
     </div>
   )
 }
 
-export default Notfound
+export default page
